0b7db924fb0eee94727393d9f01936e386468f70
Laszlo Kocsis <leslie.wilmore00@gmail.com>
2022-05-16 17:33:58 +0300

    Create dark mode functionality

--- script.js
+++ script.js
@@ -388,6 +388,30 @@ const workSectionChecker = () => {
   }, { once: true });
 }
 
+//dark mode
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+  }
+}
+
+const switchTheme = (e) => {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  }
+}
+
 // function calls
 document.addEventListener("DOMContentLoaded", () => {
   sleep(400).then(() => typeWriter(hello, 100, helloText));
@@ -402,6 +426,7 @@ window.onload = () => {
   // workSectionChecker();
   window.addEventListener('scroll', reveal);
   window.addEventListener('wheel', scrollWithWheel, { passive: false });
+  toggleSwitch.addEventListener('change', switchTheme, false);
 }
 
 // got to top of page when website is refreshed
